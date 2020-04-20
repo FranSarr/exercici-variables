95ed9a0c29de7d6e1540238f72daf5c78b5b0b95
fsarrias <fsarrias@kolab.es>
2020-04-20 15:00:22 +0200

    Subo el programa acabado

---
+++ src/fase1.java
@@ -0,0 +1,16 @@
+import static java.lang.System.out;
+
+public class fase1 {
+        public static void main(String[] args) {
+            String name= "Pepito";
+            String surname1= "Perez";
+            String surname2= "Gonzalez";
+            int day = 27;
+            int month = 12;
+            int year = 1990;
+            System.out.println(surname1 + " " + surname2 + ", "+ name);
+            System.out.println(day + "/" + month + "/" + year);
+
+        }
+
+}
